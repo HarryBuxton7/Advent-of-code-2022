c6a4b45894e29833e8fdeeb5ee92e85d601991c2
HarryBuxton7 <93356508+HarryBuxton7@users.noreply.github.com>
2022-12-02 03:58:23 +0000

    first commit

---
+++ 1.1.js
@@ -0,0 +1,23 @@
+const lineReader = require('line-reader');
+
+let currentTotal = 0
+let biggestTotal = 0
+
+lineReader.eachLine('./input.txt',(line,last)=>{
+    if (isNaN(parseInt(line)) != true){
+        currentTotal = currentTotal + parseInt(line)
+        if (biggestTotal < currentTotal){
+            biggestTotal = currentTotal
+            console.log(biggestTotal)
+        }
+    } else {
+        currentTotal = 0
+    }
+})
+
+
+setTimeout(function(){
+    //answer:
+    console.log("the answer is: " + biggestTotal)
+}, 500);
+
---
+++ 1.2.js
@@ -0,0 +1,28 @@
+const lineReader = require('line-reader');
+
+let currentTotal = 0
+let biggestTotal = 0
+
+let listOfNumbers = []
+lineReader.eachLine('./input.txt',(line,last)=>{
+    if (isNaN(parseInt(line)) != true){
+        currentTotal = currentTotal + parseInt(line)
+        if (biggestTotal < currentTotal){
+            biggestTotal = currentTotal
+            console.log(biggestTotal)
+        }
+    } else {
+        listOfNumbers.push(currentTotal)
+        currentTotal = 0
+    }
+})
+
+setTimeout(function(){
+    console.log("ended")
+    function findThreeLargestNumbers(array) {
+        return [...array].sort((a, b) => a - b).slice(-3);
+      }
+      
+    //answer:
+    console.log("the answer is: " + findThreeLargestNumbers(listOfNumbers))
+}, 500);
